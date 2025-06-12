a1a693469971dda5d2b4969290b0e4cf3e84d882
sctruong <sctruong@uci.edu>
2025-06-12 11:46:43 -0700

    added 80

--- backend/server.js
+++ backend/server.js
@@ -115,4 +115,5 @@ app.delete('/photos/:filename', (req, res) => {
 
 
 // Start
-app.listen(3000, () => console.log('Server http://localhost:3000'));
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
